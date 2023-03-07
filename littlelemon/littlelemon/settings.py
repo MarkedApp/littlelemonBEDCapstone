d6b9579735f2463854036895ade0338a044bb02c
markedApp <markedappdeveloper@gmail.com>
2023-03-08 00:35:48 +0100

    MySQL set up and admin interface working

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'llroot',
+        'PASSWORD': 'strongpwd',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
